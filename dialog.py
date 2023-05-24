dbb242344f8f3ecf0a0bc418537f35c878b5c068
Francisco-Abdala <112143533+Francisco-Abdala@users.noreply.github.com>
2023-05-24 19:40:50 -0400

    Add files via upload

---
+++ dialog.py
@@ -0,0 +1,17 @@
+import sys
+import gi
+
+gi.require_version('Gtk', '4.0')
+from gi.repository import Gtk, Gio, GObject
+
+
+class ExampleDialog(Gtk.MessageDialog):
+    def __init__(self, parent):
+        super().__init__(title="Estariamos listos", transient_for=parent)
+
+        self.add_buttons(
+            "_OK",
+            Gtk.ResponseType.OK
+        )
+
+        self.set_markup("Se guardó con éxito")
