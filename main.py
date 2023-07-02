376ed8b74cf64d801371dbb4b30f24adc2d393b6
Francisco-Abdala <112143533+Francisco-Abdala@users.noreply.github.com>
2023-07-02 00:41:31 -0400

    Main.py

--- main.py
+++ main.py
@@ -12,78 +12,68 @@ from comunidad import Comunidad
 class MainWindow(Gtk.ApplicationWindow):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.app = self.get_application()
-        self.set_default_size(100, 200)
-        header_bar = Gtk.HeaderBar.new()
-        self.set_titlebar(titlebar=header_bar)
+
+        self.set_default_size(800,250)
         self.set_title("Simulación")
+        self.box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL,spacing= 10)
+        self.set_child(self.box)    
 
-        menu_button_model = Gio.Menu()
-        menu_button_model.append("About", "app.about")
-        menu_button = Gtk.MenuButton.new()
+        self.grid1 = Gtk.GridView()
+        self.box.append(self.grid1)
+        self.button_save = Gtk.Button(label="Empezar simulación")
+        self.button_save.connect("clicked", self.on_start_button_clicked)                          
+        self.box.append(self.button_save)
 
 
-        menu_button.set_icon_name(icon_name='open-menu-symbolic')
-        menu_button.set_menu_model(menu_model=menu_button_model)
-        header_bar.pack_end(child=menu_button)
-
-        self.principal_box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL,spacing= 10)
-        self.info_box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL,spacing= 10)
-
-        self.principal_box.append(self.principal_box)
-
-        # Botón para empezar la simualción
-        self.start_button = Gtk.Button.new_with_label("Empezar simulación")
-        self.start_button.connect("clicked",self.on_start_button_clicked)
-        self.principal_box.append(self.start_button)
-
+        
     def on_start_button_clicked(self, button):
         self.iniciar_simulacion()
         self.progreso.close()
-    def iniciar_simulacion(self):
+        self.show_mensaje_inicio()
 
+    def iniciar_simulacion(self):
         infeccion_probable = 20
         promedio_pasos = 5
-        mortalidad = 3
-        enfermedad = Enfermedad(infeccion_probable,
-                                promedio_pasos, mortalidad)
+        muerte = 3
+        virus = Enfermedad(infeccion_probable,
+                                promedio_pasos, muerte)
         
-
-        num_ciudadanos = 100
+        poblacion = 100000
         infectados = 2
         media_conexion_fisica = 30
         probabilidad_conexion_fisica = 50
-        comunidad = Comunidad(num_ciudadanos, enfermedad, infectados,
+        ciudad = Comunidad(poblacion, virus, infectados,
                             media_conexion_fisica, probabilidad_conexion_fisica)
         
 
-        dias_simulacion = 50
-        simulacion = Simulacion(dias_simulacion, comunidad, enfermedad)
-        simulacion.simular()
+        dias = 50
+        simulacion = Simulacion(ciudad, virus,dias)
+        simulacion.comienzo()
 
 class MyApp(Gtk.Application):
+
     def __init__(self):
         super().__init__(application_id='cl.com.Example',
-                        flags=Gio.ApplicationFlags.FLAGS_NONE)
-        self.create_action("about", self.on_about_action)
+                         flags=Gio.ApplicationFlags.FLAGS_NONE)
 
-
-    def on_about_action(self, action, param):
-        about = Gtk.AboutDialog.new()
-        about.set_authors(["Francisco Abdala", "Amanda Pérez"])
-        about.set_program_name("Simulación")
-        about.set_copyright("Ingeniería Civil en Bioinformática")
-        about.set_visible(True)
+        self.create_action('quit', self.exit_app, ['<primary>q'])
+        self.create_action('about', self.on_about_action)
 
 
     def do_activate(self):
-        active_window = self.props.active_window
-        if active_window:
-            active_window.present()
-        else:
-            self.win = MainWindow(application=self)
-            self.win.present()
+        win = self.props.active_window
+        if not win:
+            win = MainWindow(application=self)
+        win.present()
 
+    def do_startup(self):
+        Gtk.Application.do_startup(self)
+
+    def do_shutdown(self):
+        Gtk.Application.do_shutdown(self)
+
+    def exit_app(self, action, param):
+        self.quit()
 
     def create_action(self, name, callback, shortcuts=None):
         action = Gio.SimpleAction.new(name, None)
@@ -92,6 +82,13 @@ class MyApp(Gtk.Application):
         if shortcuts:
             self.set_accels_for_action(f'app.{name}', shortcuts)
 
+
+    def on_about_action(self, action, param):
+        about = Gtk.AboutDialog.new()
+        about.set_authors(["Francisco Abdala", "Amanda Pérez"])
+        about.set_program_name("Simulación")
+        about.set_copyright("Ingeniería Civil en Bioinformática")
+        about.set_visible(True)
 
 if __name__ == '__main__':
     app = MyApp()
