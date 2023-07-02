b8c60a1bb83b74ec02f0d8467c0efe6b1534b72f
Francisco-Abdala <112143533+Francisco-Abdala@users.noreply.github.com>
2023-07-02 00:40:36 -0400

    Comunidad.py

--- comunidad.py
+++ comunidad.py
@@ -3,25 +3,26 @@ import json
 import random
 
 class Comunidad():
-    def __init__(self,parametro1,parametro2):
+    def __init__(self,parametro1,parametro2,parametro3,parametro4,parametro5):
         #Cantidad de ciudadanos
-        self.__num_ciudadanos = None
+        self.__num_ciudadanos = parametro1
 
         #Lista de objetos persona
         self.__habitantes = []
 
         #Media de conexiones fisicas
-        self.__promedio_conexion_fisica = parametro1
+        self.__promedio_conexion_fisica = parametro4
 
         #La clase enfermedad
-        self.__enfermedad = None
+        self.__enfermedad = parametro2
 
         #Número de infectados
-        self.__num_infectados = None
+        self.__num_infectados = parametro3
 
         #Probabilidad de interacción de una persona con otra
-        self.__probabilidad_conexion_fisica = parametro2
+        self.__probabilidad_conexion_fisica = parametro5
 
+        self.__fallecidos = 0
         self.crear_gente()
 
 #Métodos get para ingresar a los datos    
@@ -43,33 +44,42 @@ class Comunidad():
     def get_probabilidad_conexion_fisica(self):
         return self.__probabilidad_conexion_fisica
     
+    def get_num_infectado(self):
+        return self.__num_infectados
+    
+    def get_habitantes(self):
+        return self.__habitantes
+    
+    def get_fallecidos(self):
+        return self.__fallecidos
 
 #Crea la gente que estará en la comunidad
     def crear_gente(self):
         with open("texto.json") as archivo:
+            #Clase persona
             individuo = Persona()
-            lista_de_familia = []
+
+            #Información del archivo json
             datos = json.load(archivo)
+
             #Crea la lista de nombres 
             nombres = datos["nombres"]
+
             #Crea la lista de apellidos
             apellidos = datos["apellidos"]
-            for i in range(10):
+            
+            contador = 0
+
+            for i in range(self.__num_ciudadanos):
                 ayuda = list(set([apellidos[random.randint(0, 153)]]))
-                lista_de_familia.append(ayuda)
-                contador = 0
                 for k in range(10):
-                    contador += 10
-                    lista_de_familias = []
+                    contador +=10
                     for j in range(10):
                         contador += 1
                         individuo.set_id(contador)
                         individuo.set_nombre(nombres[random.randint(0, 153)])
-                        individuo.set_familia((lista_de_familia[i][0]))
                         individuo.set_estado("S")
-                        lista_de_familias.append(individuo)
-                        self.set_habitantes(lista_de_familias)
-                        print(lista_de_familias.get_nombre(),lista_de_familias.get_familia())
+                        self.set_habitantes(individuo)
 
     def conexiones(self):
         while True:
@@ -94,13 +104,18 @@ class Comunidad():
 
 #Métodos set para crear los datos necesarios
     def set_habitantes(self, parametro):
-        if isinstance(parametro, list):
+        if isinstance(parametro, Persona):
             self.__habitantes.append(parametro)   
         else:
-            raise TypeError("El parametro debe ser de tipo lista")
+           print("olololo")
         
     def set_promedio_conexion_fisica(self,parametro):
         if isinstance(parametro, int):
             self.__promedio_conexion_fisica = parametro
         else:
             raise TypeError("El promedio de conexion fisica debe ser un numero entero")
+        
+    def set_fallecidos(self,parametro):
+        self.__fallecidos = parametro
+    def set_num_infectados(self,parametro):
+        self.__num_infectados = parametro
