121967a5e7c8426ffbe7e3cc45dcc98f7b09a34e
Francisco-Abdala <112143533+Francisco-Abdala@users.noreply.github.com>
2023-07-03 11:35:12 -0400

    Persona.py

--- persona.py
+++ persona.py
@@ -9,19 +9,13 @@ class Persona():
         #Tipo None que al crearse el objeto será tipo string, identifica su apellido
         self.__familia = None
 
-        #Tipo None que al crearse el objeto será tipo Enfermedad, enfermedad que tiene la persona
-        self.__enfermedad = None
-
         #Cambiará entre "S" de susceptible,"I" de infectado, "R" de recuperado y "F" de fallecido, identidica el estado de la persona
-        self.__estado = None
+        self.__estado = "S"
 
         #Tipo None que al crearse el objeto será tipo int,identidica cuanto dura la enfermedad
         self.__contador = None
 
 #Métodos get para obtener el parametro que se necesite.
-    def get_comunidad(self):
-        return self.__comunidad
-    
     def get_id(self):
         return self.__id
     
@@ -30,9 +24,6 @@ class Persona():
     
     def get_familia(self):
         return self.__familia
-    
-    def get_enfermedad(self):
-        return self.__enfermedad
     
     def get_estado(self):
         return self.__estado
@@ -51,32 +42,24 @@ class Persona():
         if isinstance (parametro,str):
             self.__familia = parametro
         else:
-            print("olololo")
+            print("claro")
 
     def set_id(self,paramtetro):
         if isinstance(paramtetro,int):
             self.__id = paramtetro
         else:
-            print("olololo")
-            
-    def set_comunidad(self,parametro):
-        self.__comunidad = parametro
+            print("si")
 
     def set_estado(self, estado):
         if estado in ["S", "R", "I", "F"]:
-            self.__estado = estado
-        
-    def set_enfermedad(self,parametro):
-        self.__enfermedad = parametro
-
+            self.__estado = estado 
 
     def set_contador(self,parametro):
         if isinstance(parametro,int):
             self.__contador = parametro
         else:
-            print("olololo")
+            print("ola")
 
     #Método que indica cuantos días enfermo le queda a la persona, o en su caso, cuantos días de inmunidad tiene
     def restar_contador(self):
         self.__contador = self.__contador -1
-        
