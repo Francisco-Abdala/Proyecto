90f2c75272e497c508376fd7e8c8cdf7f77baec8
Francisco-Abdala <112143533+Francisco-Abdala@users.noreply.github.com>
2023-07-24 12:12:21 -0400

    last version simulacion.py

--- simulacion.py
+++ simulacion.py
@@ -4,6 +4,8 @@ import matplotlib.pyplot as plt
 import statsmodels.api as sm
 from time import sleep
 from persona import Persona
+from enfermedad import Enfermedad
+from comunidad import Comunidad
 class Simulacion():
     def __init__(self, parametro1,parametro2,parametro3):
         #La comunidad que tendrá la enfermedad
@@ -17,148 +19,116 @@ class Simulacion():
 
         #contador
         self.__cuenta = 0
-
         #Infectados por día
-        self.__infectados = [self.__comunidad.get_num_infectados()]
-
-        self.__enfermos = [self.__comunidad.get_num_infectados()]
+        self.__infectados = 0
         #Fallecidos por día
-        self.__fallecidos = [0]
+        self.__fallecidos = 0
 
         #Susceptibles por día
-        self.__susceptible = [self.__comunidad.get_num_ciudadanos() - self.__comunidad.get_num_infectados()]
-        
+        self.__susceptible = 0
 #Métodos get para la obtención de los datos cuando se requiera
     def get_pasos(self):
         return self.__pasos
-    
     def get_cuenta(self):
         return self.__cuenta
+    def get_enfernedad(self):
+        return self.__enfermedad
+    def get_fallecidos(self):
+        return self.__fallecidos
+    def get_susceptible(self):
+        return self.__susceptible
+    def get_infectados(self):
+        return self.__infectados
     
-    def otro_dia(self):
-        self.__cuenta += self.__cuenta
-#Comienza las infecciones
-    def comienzo(self):
-        for _ in range(self.__pasos):
-            #Considera el día cero
-            if self.__cuenta == 0:
-                self.caso_cero()
-                sleep(2)
-            #Si es otro día,cambia de día
-            else:
-                self.pasar_el_dia()
-
-            self.__cuenta += 1
-            self.datos()
-
-#Creación del primer caso
-    def caso_cero(self):
-        caso_0 = self.__comunidad.get_num_infectados()
-        poblacion = self.__comunidad.get_num_ciudadanos()
-        personas = self.__comunidad.get_habitantes()
-        for _ in range(caso_0):
-            while True:
-                ayuda = random.randint(0, poblacion - 1)
-                if personas[ayuda].get_estado() == "S":
-                    personas[ayuda].set_estado("I")
-                    personas[ayuda].set_contador(self.__enfermedad.set_contador())
-                    break
-                else:
-                    ayuda = None
-
-        for persona in range(len(personas)-1):
-            ayuda = personas[persona]
-            print(type(ayuda))
-            self.__comunidad.set_habitantes(ayuda)
-
-#Muestra el avance y cambio de la enfermedad en la poblacion
-    def datos(self):
-        dia = self.__cuenta
-        contagio = self.__comunidad.get_num_infectados()
-        infectados = self.__comunidad.get_num_infectados()
-        fallecidos = self.__comunidad.get_fallecidos()
-        print(f"Día: {dia}, contagiados totales: {contagio}, enfermos: {infectados}, muertos {fallecidos}\n")
-
-#Guarda los datos en las variables necesarias
-    def leer_datos(self):
-        fallecidos = 0
-        infectados = 0
-        recuperados = 0
-        susceptibles = 0
-        #Compara sus estados para obtener cuantos hay de cada uno
-        for ciudadano in self.__comunidad.get_habitantes():
-            if ciudadano.get_estado() == "F":
-                fallecidos +=1
-            elif ciudadano.get_estado() == "I":
-                infectados +=1
-            elif ciudadano.get_estado() == "R":
-                recuperados += 1
-            elif ciudadano.get_estado() == "S":
-                susceptibles +=1
-
-        #Agrega los datos donde deben estar
-        self.__comunidad.set_fallecidos(fallecidos)
-        self.__fallecidos.append(fallecidos)
-
-        self.__comunidad.set_enfermos(infectados)
-        self.__enfermos.append(infectados)
-
-        self.__comunidad.set_num_infectados(fallecidos+infectados+recuperados)
-        self.__infectados.append(fallecidos+infectados+recuperados)
-
-
-        self.__susceptible.append(susceptibles)
-
-#Crea la red de contagios
-    def contagiar(self):
-        enfermos = []
-        for ciudadano in self.__comunidad.get_habitantes():
-            if ciudadano.get_estado() == "I":
-                conexiones = self.__comunidad.conexiones()
-                for _ in range(conexiones):
-                    if self.__comunidad.contacto_estrecho():
-                            enfermo = self.__comunidad.contagiar_contacto_estrecho(ciudadano)
-                            if isinstance(enfermo,Persona):
-                                enfermos.append(enfermo)
-                    else:
-                        if self.__enfermedad.contagio():
-                            nuevo_enfermo = self.__comunidad.contagiar_random()
-                            if isinstance(nuevo_enfermo, Persona):
-                                enfermos.append(nuevo_enfermo)
-        for i in range(len(enfermos)):
-            enfermos[i].set_estado("I")
-
-
-
-
-#Verifica si siguen enfermos,luego comparan cuantos días llevan con la enfermedad, si pasaron todos los días, determina si esta muerto o recuperado
-    def continua_enfermo(self):
-        for ciudadano in self.__comunidad.get_habitantes():
-            if ciudadano.get_estado() == "I":
-                ciudadano.restar_contador()
-                if ciudadano.get_contador() == 0:
-                    if self.__enfermedad.muerte():
-                        ciudadano.set_estado("F")
-                    else:
-                        ciudadano.set_estado("R")
-
-#Muestra el gráfico de la enfermedad
-    def mostrar_grafico(self):
-        plt.show()
-        plt.plot(self.__enfermos)
-        plt.plot(self.__infectados)
-        plt.plot(self.__fallecidos)
-        plt.plot(self.__susceptible)
-        plt.grid() 
-        plt.xlabel('Días')
-        plt.ylabel('Población')
-
-        if self.__pasos== self.__cuenta:
-            plt.title(f"Gráfico Modelo SIR Final de la sumlación ({self.__pasos} días)")
+#Métodos set para la creación de datos cuando se requiera
+    def set_infectados(self,parametro):
+        if isinstance(parametro,int):
+            self.__infectados = parametro
         else:
-            plt.title(f"Gráfico Modelo SIR día {self.__cuenta}")
-#Cambia de día
-    def pasar_el_dia(self):
-        self.continua_enfermo()
-        self.contagiar()
-        self.leer_datos()
+            raise ValueError("No es un número")
+    def set_fallecidos(self,parametro):
+        if isinstance(parametro,int):
+            self.__fallecidos = parametro
+        else:
+            raise ValueError("No es un número")
+    def set_susceptible(self,parametro):
+        if isinstance(parametro,int):
+            self.__susceptible = parametro
+        else:
+            raise ValueError("No es un número")
+
+#Inicia simulación
+    def comienzo(self):
+       contador = 0
+       for i in range(self.__pasos):
+            contador += 1
+            #Cantidad de personas susceptibles
+            susceptibles = self.__comunidad.get_num_ciudadanos()
+
+            #Cantidad de personas infectadas
+            infectados = self.__comunidad.get_num_infectados()
+
+            #Cantidad de personas recuperadas
+            recuperados = self.__comunidad.get_recuperados()
+
+            #Capacidad de transmisión de la enfermedad
+            transmision = self.__enfermedad.get_infeccion_probable()
+
+            #Tasa de recuperación
+            recuperacion = 1
+
+            #Tasa de mortalidad
+            muerte = self.__enfermedad.get_muerte()
+
+            #Cantidad de muertos
+            muertos = self.__comunidad.get_muertos()
+
+            #Formulas del modelo SIR
+            ayuda_s = susceptibles - (transmision * susceptibles * infectados)
+            ayuda_i = infectados + (transmision * susceptibles * infectados) - (recuperacion * infectados) -  (muerte * infectados)
+            ayuda_r = recuperados + (recuperacion * infectados)
+            ayuda_m = muertos + (muerte * infectados)
+
+            #Personas susceptibles
+            for i in range(self.__comunidad.get_num_ciudadanos()):
+                for _ in self.__comunidad.get_habitantes():
+                    if self.__comunidad.get_habitantes()[i-1].get_estado() == "S":
+                        self.__comunidad.set_num_susceptibles(1)
+
+            #Personas infectadas
+            for i in range(self.__comunidad.get_num_ciudadanos()):
+                for _ in self.__comunidad.get_habitantes():
+                    if self.__comunidad.get_habitantes()[i-1].get_estado() == "S":
+                        self.__comunidad.get_habitantes()[i-1].set_estado("I")
+                        self.__comunidad.set_num_infectados(1)
+                        self.__comunidad.set_num_susceptibles(-1)
+
+                
+            #Personas recuperadas
+            for i in range(self.__comunidad.get_num_ciudadanos()):
+                for _ in self.__comunidad.get_habitantes():
+                    if self.__comunidad.get_habitantes()[i-1].get_estado() == "I" and contador >=3:
+                        ayuda = random.choice(0,1,2)
+                        a = self.__comunidad.get_vacunas()[ayuda]
+                        self.__comunidad.get_habitantes()[i-1].a.vacunado()
+                        self.__comunidad.set_num_recuperados(1)
+                        self.__comunidad.set_num_susceptibles(-1)
+                        self.__comunidad.set_num_infectados(-1)
+                        if self.__comunidad.get_vacunas()[ayuda].get_vacunas_disponibles() == 0:
+                            self.__comunidad.get_vacunas().pop(ayuda)
+
+
+            #Personas muertas
+            for i in range(self.__comunidad.get_num_ciudadanos()):
+                for _ in self.__comunidad.get_habitantes():
+                    if self.__comunidad.get_habitantes()[i-1].get_estado() == "I" and self.__comunidad.get_habitantes()[i].get_vivencia() == True:
+                        self.__comunidad.get_habitantes()[i-1].set_estado("F")
+                        self.__comunidad.get_habitantes()[i-1].set_vivencia(False)
+                        self.__comunidad.set_num_infectados(-1)
+                        self.__comunidad.set_num_muertos(1)
+                
+            
+
+            print(f"Día: {contador}, poblacion: {self.__comunidad.get_num_ciudadanos()}, infectados: {self.__comunidad.get_num_infectado()}, recuperados: {self.__comunidad.get_recuperados()}, fallecidos: {self.__comunidad.get_fallecidos()}")
+
+        
