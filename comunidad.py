bf7fa68c01eb9924a7d41df9a9f0fc5a45816a0d
Francisco-Abdala <112143533+Francisco-Abdala@users.noreply.github.com>
2023-07-24 12:10:45 -0400

    last version comunidad.py

--- comunidad.py
+++ comunidad.py
@@ -23,45 +23,80 @@ class Comunidad():
         #Número de infectados
         self.__num_infectados = parametro3
 
-
-        self.__enfermos = parametro3
-
         #Probabilidad de interacción de una persona con otra
         self.__probabilidad_conexion_fisica = parametro5
 
-        self.__fallecidos = 0
+        #Número de recuperados
+        self.__recuperados = 0
 
+        #Número de susceptibles
+        self.__susceptibles = parametro1
+
+        #Número de muertos
+        self.__muertos = 0
+        #Función que crea gente
+        self.__vacunas = []
         self.crear_gente()
 
 #Métodos get para ingresar a los datos    
     def get_num_ciudadanos(self):
         return self.__num_ciudadanos
-    
     def get_promedio_conexion_fisica(self):
-        return self.__promedio_conexion_fisica
-    
-
+        return self.__promedio_conexion_fisica 
     def get_enfermedad(self):
         return self.__enfermedad
-    
     def get_num_infectados(self):
-        return self.__num_infectados
-        
+        return self.__num_infectados       
     def get_enfermos(self):
         return self.__enfermos
-
     def get_probabilidad_conexion_fisica(self):
         return self.__probabilidad_conexion_fisica
-    
     def get_num_infectado(self):
         return self.__num_infectados
-    
     def get_habitantes(self):
         return self.__habitantes
-    
     def get_fallecidos(self):
-        return self.__fallecidos
-
+        return self.__muertos
+    def get_recuperados(self):
+        return self.__recuperados
+    def get_susceptible(self):
+        return self.__susceptibles
+    def get_muertos(self):
+        return self.__muertos
+    def get_vacunas(self):
+        return self.__vacunas
+#Métodos set para crear los datos necesarios
+    def set_habitantes(self, parametro):
+        if isinstance(parametro, Persona):
+            self.__habitantes.append(parametro)   
+        else:
+           print("que")    
+    def set_promedio_conexion_fisica(self,parametro):
+        if isinstance(parametro, int):
+            self.__promedio_conexion_fisica = parametro
+        else:
+            raise TypeError("El promedio de conexion fisica debe ser un numero entero")
+    def set_fallecidos(self,parametro): 
+        self.__fallecidos += parametro
+    def set_num_infectados(self,parametro):
+        self.__num_infectados = self.__num_infectados + parametro
+    def set_num_recuperados(self,parametro):
+        if isinstance(parametro, int):
+            self.__recuperados = self.__recuperados + parametro
+        else:
+            raise ValueError("No es un número")
+    def set_num_susceptibles(self,parametro):
+        if isinstance(parametro, int):
+            self.__susceptibles = self.__susceptibles + parametro
+        else:
+            raise ValueError("No es un número")
+    def set_num_muertos(self,parametro):
+        if isinstance(parametro, int):
+            self.__muertos = self.__muertos + parametro
+        else:
+            raise ValueError("No es un número")
+    def set_vacuna(self,parametro):
+        self.__vacunas.append(parametro)
 #Crea la gente que estará en la comunidad
     def crear_gente(self):
             #Clase persona
@@ -79,54 +114,5 @@ class Comunidad():
                 individuo.set_id(contador)
                 individuo.set_nombre(nombres[random.randint(0, 153)])
                 individuo.set_familia(apellidos[random.randint(0,153)])
-                print(type(individuo))
+                individuo.set_estado("S")
                 self.set_habitantes(individuo)
-                #print(contador)
-
-#Determina cuantas conexiones habrán
-    def conexiones(self):
-        while True:
-            conexion = random.gauss(self.__promedio_conexion_fisica, self.__promedio_conexion_fisica/4)
-            if conexion >= 0 and conexion < self.__promedio_conexion_fisica*4:
-                break
-        return int(conexion)
-
-#Contagia a alguien aleatoriamente
-    def contagiar_random(self):
-        print("c")
-        while True:
-            ayuda = random.randint(0, self.__num_ciudadanos - 1)
-            individuo = self.__habitantes[ayuda]
-            if individuo.get_estado() == "S":
-                individuo.set_contador(self.__enfermedad.set_contador())
-                return individuo
-            elif individuo.get_estado() in ["I", "R"]:
-                return None
-    def contacto_estrecho(self):
-        random_number = random.randint(1, 100)
-        if random_number <= self.__probabilidad_conexion_fisica():
-            return True
-        return False
-
-#Métodos set para crear los datos necesarios
-    def set_habitantes(self, parametro):
-        if isinstance(parametro, Persona):
-            self.__habitantes.append(parametro)   
-        else:
-           print("que")
-        
-    def set_promedio_conexion_fisica(self,parametro):
-        if isinstance(parametro, int):
-            self.__promedio_conexion_fisica = parametro
-        else:
-            raise TypeError("El promedio de conexion fisica debe ser un numero entero")
-        
-    def set_fallecidos(self,parametro): 
-        self.__fallecidos = parametro
-
-
-    def set_num_infectados(self,parametro):
-        self.__num_infectados = parametro
-
-    def set_enfermos(self,parametro):
-        self.__enfermos = parametro
